335a9a866f35198d57184132e2d22667de0b4fe7
Jedidta Adoni Saputra <adoni.091203@gmail.com>
2024-12-08 15:45:20 +0700

    feat: pasang image untuk select chapter

--- Assets/Scripts/Chapter/LevelButton.cs
+++ Assets/Scripts/Chapter/LevelButton.cs
@@ -3,21 +3,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 
 [System.Serializable]
 public class LevelButton
 {
     public string chapterName;
-    public Button button;
-    public Color enabledColor;
-    public Color disabledColor;
+    public GameObject chapterSelector;
+    // public Color enabledColor;
+    // public Color disabledColor;
 
     public void UpdateButtonStatus()
     {
         GameChapter gameChapter = GameObject.FindObjectOfType<GameChapter>();
+        Button button = chapterSelector.GetComponent<Button>();
         bool isComplete = gameChapter.IsChapterComplete(chapterName);
-        button.interactable = isComplete;
-        button.image.color = isComplete? enabledColor : disabledColor;
+
+        if(chapterSelector != null) {
+            Transform activeChild = chapterSelector.transform.Find("Active");
+            Transform inactiveChild = chapterSelector.transform.Find("Inactive");
+
+            button.interactable = isComplete;
+
+            if (isComplete)
+            {
+                if (activeChild != null) {
+                    activeChild.gameObject.SetActive(true);
+                }
+                if (inactiveChild != null) {
+                    inactiveChild.gameObject.SetActive(false);
+                }
+            }
+            else
+            {
+                if (activeChild != null) {
+                    activeChild.gameObject.SetActive(false);
+                }
+                if (inactiveChild != null) {
+                    inactiveChild.gameObject.SetActive(true);
+                }
+            }
+        }
     }
 }
