19a430c968138499b8a3764a0c7ef3ef9cc3cee3
Jedidta Adoni Saputra <adoni.091203@gmail.com>
2024-12-11 05:05:11 +0700

    fix: resolve conflict in level 1

--- Assets/Scripts/Pause & Option/pauseMenu.cs
+++ Assets/Scripts/Pause & Option/pauseMenu.cs
@@ -4,28 +4,20 @@ using UnityEngine.UIElements;
 
 public class pauseMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject _pauseMenu;
-    [SerializeField] private GameObject _optionMenu;
-
     private void Start()
     {
-        _pauseMenu.SetActive(false);
-        _optionMenu.SetActive(false);
         Time.timeScale = 1;
     }
     public void pauseGame()
     {
-        _pauseMenu.SetActive(true);
         Time.timeScale = 0;
     }
     public void Resume()
     {
-        _pauseMenu.SetActive(false);
         Time.timeScale = 1;
     }
     public void option()
     {
-        _optionMenu.SetActive(true);
         Time.timeScale = 0;
     }
     public void mainMenu()
